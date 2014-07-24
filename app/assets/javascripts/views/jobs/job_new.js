234ab93fa5f0e2f7c7722c8e5a42571e2a3c8d56
Sierra <sierrabrown808@gmail.com>
2014-07-23 18:45:58 -0700

    Jobs can be saved to db successfully.

--- app/assets/javascripts/views/jobs/job_new.js
+++ app/assets/javascripts/views/jobs/job_new.js
@@ -1,6 +1,24 @@
 TR.Views.JobNew = Backbone.View.extend({
 	template: JST['jobs/new'],
 	
+	events: {
+		"submit form": "submit"
+	},
+	
+	submit: function(event){
+		event.preventDefault();
+		var params = $(event.currentTarget).serializeJSON()
+		debugger;
+		var newJob = new TR.Models.Job(params["job"])
+		newJob.save({}, {
+			success: function() {
+				TR.jobs.add(newJob);
+				alert("Job sent out!");
+				this.render()
+			}
+		})
+	},
+	
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
