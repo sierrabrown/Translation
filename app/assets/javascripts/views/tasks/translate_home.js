0417b456a9529506e4ae253ac8d7b277e7b5e906
Sierra <sierrabrown808@gmail.com>
2014-07-24 18:18:25 -0700

    Translation Edit page appears prefilled with editable text from the db.

--- app/assets/javascripts/views/tasks/translate_home.js
+++ app/assets/javascripts/views/tasks/translate_home.js
@@ -1,10 +1,16 @@
 TR.Views.TranslateHome = Backbone.View.extend({
 	template: JST['tasks/home'],
-	
+	workspace: JST['tasks/edit'],
 	
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
+		return this;
+	},
+	
+	renderWorkspace: function() {
+		var content = this.workspace();
+		this.$el.html(content)
 		return this;
 	},
 	
@@ -16,12 +22,21 @@ TR.Views.TranslateHome = Backbone.View.extend({
 		event.preventDefault();
 		var params = $(event.currentTarget).serializeJSON()
 		var params = params["choice"]
+		var that = this;
 		TR.tasks.fetch({ 
 			data: {source_lang: params["from"], target_lang: params["to"]},
-			success: function(){ console.log(TR.tasks)}
+			success: function() { that.edit() }
 		})
-		
+	},
+	
+	edit: function() {
+		var model = TR.tasks.models[0]
+		if (model != undefined) {
+			var content = this.workspace( {task: model });
+			this.$("#workspace").html(content)
+		}
 	}
+	
 	
 	
 })
