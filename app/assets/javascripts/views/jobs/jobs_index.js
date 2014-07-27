fe136de2400d8f24ba1c83aad53c80145afdda9c
Sierra <sierrabrown808@gmail.com>
2014-07-27 15:06:35 -0700

    middle of updating points via a form

--- app/assets/javascripts/views/jobs/jobs_index.js
+++ app/assets/javascripts/views/jobs/jobs_index.js
@@ -6,9 +6,38 @@ TR.Views.JobsIndex = Backbone.View.extend({
 		this.listenTo(this.model, 'sync', this.render)
 	},
 	
+	events: {
+		"submit form": "submit",
+	},
+	
 	render: function() {
 		var content = this.template( {jobs: this.collection, tasks: this.collection2, currentUser: this.model} );
 		this.$el.html(content);
 		return this;
-	}
+	},
+	
+	submit: function(event){
+		event.preventDefault();
+		event.stopPropagation();
+		var that = this
+		var params = $(event.target).serializeJSON()
+		var params = params["user"]
+		// Parde int and then move on
+		this.model.set({target_text: params["target_text"], status: 'completed', translator_id: TR.currentUser.id})
+	
+		this.model.save({}, {
+			success: function() {
+				TR.tasks.fetch({
+					data: {source_lang: that.model.source_lang, target_lang: that.model.target_lang, status: 'in progress'},
+					success: function() { 
+						if (TR.tasks.length == 0) {
+							alert('You finished all the tasks available for this language pair.')
+						} else {
+						that.render()
+						} 
+					}
+				})
+			}
+		})
+	},
 })
