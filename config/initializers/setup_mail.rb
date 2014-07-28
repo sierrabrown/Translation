6193cad5add5d689e34a9fcf8e5581e0e31aaa7a
Sierra <sierrabrown808@gmail.com>
2014-07-28 16:42:20 -0700

    undo

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port       => 587,
   :domain     => "gmail.com",
   :username   => "sierrabrown808@gmail.com",
-  :password   => "Torin121",
+  :password   => "",
   :authentication => "plain",
   :enable_starttls_auto => true
 }
