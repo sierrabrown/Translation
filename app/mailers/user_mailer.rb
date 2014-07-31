fc7b89cd75eb7c450a3259f839b3b133261fddc1
Sierra <sierrabrown808@gmail.com>
2014-07-31 16:22:07 -0700

    includes attachments so sweet

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,12 +6,13 @@ class UserMailer < ActionMailer::Base
   def completed_job(user, job)
     @job = job
     @user = user
+
+    translation_pdf_view = JobPdf.new(@job)
+    translation_pdf_content = translation_pdf_view.render(@job)
+
+    attachments["#{@job.title}.pdf"] = {mime_type: 'application/pdf', content: translation_pdf_content}
+    
     mail to: @job.email, subject: "You translated text #{@job.title} is attached."
-    # translation_pdf_view = JobPdf.new(@job)
-    # translation_pdf_content = translation_pdf_view.render(@job)
-    #
-    # attachments["translation_#{@job.title}"] = {mime_type: 'application/pdf', content: translation_pdf_content}
-    #
     # mail(to: @job.email, subject: "Your translated text #{@job.title} is attached.")
   end
 end
