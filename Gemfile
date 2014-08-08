3e1ae85623ed3af1efd36291b08e7957df58a9ab
Sierra <sierrabrown808@gmail.com>
2014-08-08 15:22:16 -0700

    rspec

--- Gemfile
+++ Gemfile
@@ -49,17 +49,11 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'letter_opener'
+  gem 'rspec-rails'
 end
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
-
+group :test do
+  gem 'guard-rspec'
+  gem 'capybara'
+  gem 'launchy'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,46 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara/rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+end
+
+def sign_up(username)
+  visit "/session/new"
+  click_link "signUpLink"
+  fill_in "username", with: username
+  fill_in "email", with: 'email@email.com'
+  fill_in "password", with: 'abcdef'
+  click_button 'submitButton'
+end
+
+def sign_up_as_hello_world
+  sign_up("hello_world")
+end
+
+def sign_in(username)
+  visit "/session/new"
+  fill_in "Username", with: username
+  fill_in "Password", with: 'abcdef'
+  click_button 'Sign In'
+end
+
